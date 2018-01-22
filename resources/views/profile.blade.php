97ae502cbac2142aa13bef9572e6c08b706cf876
Emiliano Carrizo Vega <emi.carrizo94@gmail.com>
2018-01-21 21:54:24 -0300

    Actualizada vista de perfil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,31 +3,37 @@
 @section('section')
         
         {{ csrf_field() }}
-  <div class="col-lg-6">
-    <fieldset disabled>
-    <div class="nombre" style="padding-bottom: 60px">
-			<label for="disabledSelect" class="col-sm-4 control-label">Nombre</label>
-			<div class="col-sm-8">
-        <input class="form-control" placeholder="{{ Auth::user()->name}}" name="nombre">
-      </div>
-    </div>
+		
+	<div class="col-lg-6 col-md-offset-3">
+		@section ('panel2_panel_title', 'Datos de Usuario')
+		@section ('panel2_panel_body')
+			<div class="col-lg-12">
+				<fieldset disabled>
+				<div class="nombre" style="padding-bottom: 50px">
+					<label for="disabledSelect" class="col-sm-3 control-label">Nombre</label>
+					<div class="col-sm-8">
+						<strong><p class="text-info">{{ Auth::user()->name}}</p></strong>
+					</div>
+				</div>
 
-    <div class="email" style="padding-bottom: 60px">
-      <label for="disabledSelect" class="col-sm-4 control-label">Email</label>
-      <div class="col-sm-8">
-        <input class="form-control" placeholder="{{ Auth::user()->email}}" name="nombre" color="black">
-      </div>
-    </div>
+				<div class="email" style="padding-bottom: 50px">
+				  <label for="disabledSelect" class="col-sm-3 control-label">Email</label>
+				  <div class="col-sm-8">
+					<strong><p class="text-info">{{ Auth::user()->email}}</p></strong>
+				  </div>
+				</div>
 
-    <div class="rol" style="padding-bottom: 8px">
-      <label for="disabledSelect" class="col-sm-4 control-label">Rol</label>
-      <div class="col-sm-8">
-        <select id="disabledSelect" class="form-control">
-          <option>{{ Auth::user()->rol->descripcion_rol}}</option>
-        </select>
-      </div>
+				<div class="rol" style="padding-bottom: 8px">
+				  <label for="disabledSelect" class="col-sm-3 control-label">Rol</label>
+				  <div class="col-sm-8">
+					<strong><p class="text-info">{{ Auth::user()->rol->descripcion_rol}}</p></strong>
+				  </div>
+				</div>
+				</fieldset>
+			  </div>
+
+
+		@endsection
+		@include('widgets.panel', array('class'=>'primary', 'header'=>true, 'as'=>'panel2'))
     </div>
-    </fieldset>
-  </div>
-            
 @stop
