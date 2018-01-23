284ae8e9bccfc06686e699758a26f7b99e75b41d
marianobarragan <mariano94b@gmail.com>
2018-01-23 15:22:02 -0300

    rango de un mes para pedir un turno en datepicker

--- resources/js/datepicker.js
+++ resources/js/datepicker.js
@@ -1,3 +1,48 @@
+/* Inicialización en español para la extensión 'UI date picker' para jQuery. */
+/* Traducido por Vester (xvester@gmail.com). */
+( function( factory ) {
+    if ( typeof define === "function" && define.amd ) {
+
+        // AMD. Register as an anonymous module.
+        define( [ "../widgets/datepicker" ], factory );
+    } else {
+
+        // Browser globals
+        factory( jQuery.datepicker );
+    }
+}( function( datepicker ) {
+
+datepicker.regional.es = {
+    closeText: "Cerrar",
+    prevText: "&#x3C;Ant",
+    nextText: "Sig&#x3E;",
+    currentText: "Hoy",
+    monthNames: [ "Enero","Febrero","Marzo","Abril","Mayo","Junio",
+    "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre" ],
+    monthNamesShort: [ "Enero","Febrero","Marzo","Abril","Mayo","Junio",
+    "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre" ],
+    dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+    dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+    dayNamesMin: [ "D","L","M","X","J","V","S" ],
+    weekHeader: "Sm",
+    dateFormat: "dd/mm/yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.es );
+
+return datepicker.regional.es;
+
+} ) );
+
+$.datepicker.setDefaults(
+  $.extend(
+    {'dateFormat':'dd-mm-yy'},
+    $.datepicker.regional['es']
+  )
+);
+
 $( function() {
 	$( "#fecha_nacimiento" ).datepicker({
     	changeMonth: true,
@@ -8,11 +53,14 @@ $( function() {
     	changeYear: true
     });
 	$( "#fecha_desde" ).datepicker({
+        minDate: new Date(),
     	changeMonth: true,
     	changeYear: true
     });
 	$( "#fecha_hasta" ).datepicker({
-    	changeMonth: true,
-    	changeYear: true
+    	minDate: new Date(),
+        changeMonth: true,
+    	changeYear: true,
+        maxDate: '+1M'
     });
 } );
